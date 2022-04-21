d67f2c985c298adbd15c38611a6ca325b29666f4
seetoe <kevinseetoe@gmail.com>
2022-04-21 15:43:35 -0400

    remove progressive web app related artifacts

--- src/ClientApp/src/index.js
+++ src/ClientApp/src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -13,8 +12,3 @@ root.render(
     <App />
   </BrowserRouter>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
